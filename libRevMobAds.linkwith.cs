a32ad8feaa937d0987f2e836dbb69f0291d7c954
RobertFoll <rrfsgh@gmail.com>
2016-01-04 20:20:52 -0500

    Update libRevMobAds.linkwith.cs

--- libRevMobAds.linkwith.cs
+++ libRevMobAds.linkwith.cs
@@ -1,3 +1,3 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("libRevMobAds.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.ArmV7s, SmartLink = true, ForceLoad = true, Frameworks = "MediaPlayer AdSupport StoreKit SystemConfiguration")]
+[assembly: LinkWith ("libRevMobAds.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.ArmV7s, SmartLink = true, ForceLoad = true, Frameworks = "MediaPlayer AdSupport StoreKit SystemConfiguration CoreLocation")]
